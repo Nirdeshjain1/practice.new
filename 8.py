2dbd4c90205e6e04b82612691a52154706fa4809
Nirdeshjain1 <136813942+Nirdeshjain1@users.noreply.github.com>
2023-11-20 10:41:51 +0530

    Add files via upload

--- 8.py
+++ 8.py
@@ -1,3 +1,5 @@
 import numpy as np 
 arr=np.linspace(5,12)
-print(arr)
+arr1=arr.copy()
+arr2=np.array([[1,2,3],[4,5,6]])
+print(arr2.shape)
---
+++ 9.py
@@ -0,0 +1,4 @@
+import numpy as np 
+arr=np.array([[[1,2],[4,5]],[[5,6],[7,8]]])
+print(arr)
+print(arr.ndim)
